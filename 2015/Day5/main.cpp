3260fc628791c4d6d24a41166fa42eb8e83e9a5d
Natey71 <natedog7120@gmail.com>
2025-12-02 19:24:57 -0500

    day5 task1 of 2015

---
+++ 2015/Day5/main.cpp
@@ -0,0 +1,66 @@
+/*
+    Conditions of Satisfaction
+    - Contains at least three vowels (aeiou only)
+    - Contains at lease one letter that appears twice in a row
+    - Does not contain the strings "ab", "cd", "pq", or "xy"
+*/
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+bool hasDoubleLetter(const std::string &s) {
+    for(size_t i = 1; i < s.length(); i++) {
+        if(s[i] == s[i-1]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    std::ifstream file("file.txt");
+    if(!file.is_open()) {
+        std::cerr << "Error opening file." << std::endl;
+        return 1;  
+    }
+    std::string line;
+    std::vector<std::string> vowels = {"a", "e", "i", "o", "u"};
+    std::vector<std::string> forbidden = {"ab", "cd", "pq", "xy"}; 
+
+    std::vector<std::string> goodStrings;
+
+    while(std::getline(file, line)) {
+        if(line.find(forbidden[0]) != std::string::npos ||
+           line.find(forbidden[1]) != std::string::npos ||
+           line.find(forbidden[2]) != std::string::npos ||
+           line.find(forbidden[3]) != std::string::npos) {
+            //std::cout << "Contains forbidden substring." << line << std::endl;
+            continue;
+        }
+        if(!hasDoubleLetter(line)) {
+            //std::cout << "Does not have double letter: " << line << std::endl;
+            continue;
+        }
+
+        int vowelCount = 0;
+        for(char c : line) {
+            for(const std::string& v : vowels) {
+                if(c == v[0]) {
+                    vowelCount++; 
+                }
+            }
+            if(vowelCount >= 3) {
+                goodStrings.push_back(line);
+                break; 
+            }
+        }
+
+    }
+    /*
+    for(std::string s : goodStrings) {
+        std::cout << "Good string: " << s << std::endl;
+    }
+    */
+    std::cout << "Total good strings: " << goodStrings.size() << std::endl; 
+    return 0;
+}
--- 2015/readfile.cpp
+++ 2015/readfile.cpp
@@ -7,6 +7,7 @@ int main() {
         std::cerr << "Error opening file." << std::endl;
         return 1;  
     }
+    std::string line;
     while(std::getline(file, line)) {
         std::cout << line << std::endl;
     }
